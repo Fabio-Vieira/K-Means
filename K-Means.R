6df40200fd4c33e2c0be25994a1d875da30e753c
Fábio Vieira <fabiovieira@id.uff.br>
2019-03-18 20:50:35 -0300

    Update K-Means.R

--- K-Means.R
+++ K-Means.R
@@ -1,6 +1,6 @@
 #####################Clustering
 #####K-Means
-###Simulation
+###A simple simulation to make me believe k-means really work
 
 data <- data.frame(A = rnorm(100, mean = c(-3, 0, 3)),
 			 B = rnorm(100, mean = c(10, 0, 3)),
@@ -21,14 +21,20 @@ km$centers #it is almost the same as the matrix centroid
 library(factoextra)
 library(cluster)
 
+#Preparing the data
 absent <- data.frame(read.csv("Absenteeism_at_work.csv", sep = ";", header = T))
 head(absent)
 plot(absent$Service.time, absent$Absenteeism.time.in.hours)
 dt <- scale(cbind(absent$Service.time, absent$Absenteeism.time.in.hours))
-fviz_nbclust(dt, FUNcluster = kmeans, method = "silhouette") #with both elbow and silhouette methods
 
-km <- kmeans(dt, centers = 2, nstart = 25)
+#Detecting the optimal number of clusters
+fviz_nbclust(dt, FUNcluster = kmeans, method = "silhouette") 
+
+#by setting nstart as 25, the algorithm chooses different random initial centroids 25 times and only returns "the best one
+km <- kmeans(dt, centers = 2, nstart = 25) 
 km$centers
 pred <- km$cluster
+
+#Plot to see the clusters
 clusplot(dt, pred, xlab = "Absenteeism", ylab = "Service Time",
          main = "Absenteeism time", lines = 0, shade = T, color = T)
